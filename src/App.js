f202cd343c28b834f87d894fed48fad4c1d8e54b
Danny JeanLouis <dlouis3386@gmail.com>
2022-03-06 12:09:52 -0500

    add library ui

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 
 import Player from "./components/Player";
 import Song from "./components/Song";
+import Library from "./components/Library";
 
 import data from "./util";
 
@@ -20,6 +21,7 @@ function App() {
         isPlaying={isPlaying}
         currentSong={currentSong}
       />
+      <Library songs={songs} />
     </div>
   );
 }
